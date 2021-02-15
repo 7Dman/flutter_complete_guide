8b1b12df651521af1394b7dd5d1404e133eccca0
Daiki Iriguchi <iriguchi@filmingo.app>
2021-02-15 15:25:32 +0900

    update delete button depends on the screen width

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -53,13 +53,19 @@ class TransactionList extends StatelessWidget {
                   ),
                   subtitle: Text(
                       DateFormat.yMMMMd().format(transactions[index].date)),
-                  trailing: IconButton(
-                    icon: Icon(
-                      Icons.delete,
-                      color: Theme.of(context).errorColor,
-                    ),
-                    onPressed: () => deleteTx(transactions[index].id),
-                  ),
+                  trailing: MediaQuery.of(context).size.width > 460
+                      ? FlatButton.icon(
+                          label: Text('Delete'),
+                          textColor: Theme.of(context).errorColor,
+                          icon: Icon(Icons.delete),
+                          onPressed: () => deleteTx(transactions[index].id))
+                      : IconButton(
+                          icon: Icon(
+                            Icons.delete,
+                            color: Theme.of(context).errorColor,
+                          ),
+                          onPressed: () => deleteTx(transactions[index].id),
+                        ),
                 ),
               );
               //　Replacing below with ListTile
