a5f3f07bd7bd7567677a6f61cdd304a4dd39ba85
Daiki Iriguchi <iriguchi@filmingo.app>
2021-02-16 01:22:16 +0900

    finalize meal detail screen

--- lib/main.dart
+++ lib/main.dart
@@ -33,6 +33,16 @@ class MyApp extends StatelessWidget {
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
+      // onGenerateRoute: (settings) {
+      //   print(settings.arguments);
+      //   if (settings.name = '/something-else') {
+      //     ...
+      //   }
+      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      // },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
+      },
     );
   }
 }
