58ad02c3f08e910298e688e3f76fb7cf0615f1a5
Daiki Iriguchi <iriguchi@filmingo.app>
2021-02-16 12:39:03 +0900

    update maindrawer and add filters screen

--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/filters_screen.dart';
 
 class MainDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Widget buildListTile(String title, IconData icon) {
+    Widget buildListTile(String title, IconData icon, Function tapHandler) {
       return ListTile(
         leading: Icon(
           icon,
           size: 26,
         ),
         title: Text(
-          'Meals',
+          title,
           style: TextStyle(
-              fontFamily: title, fontSize: 24, fontWeight: FontWeight.bold),
+              fontFamily: 'RobotoCondensed',
+              fontSize: 24,
+              fontWeight: FontWeight.bold),
         ),
-        onTap: () {},
+        onTap: tapHandler,
       );
     }
 
@@ -38,8 +41,12 @@ class MainDrawer extends StatelessWidget {
           SizedBox(
             height: 20,
           ),
-          buildListTile('Meals', Icons.restaurant),
-          buildListTile('Filters', Icons.settings),
+          buildListTile('Meals', Icons.restaurant, () {
+            Navigator.of(context).pushReplacementNamed('/');
+          }),
+          buildListTile('Filters', Icons.settings, () {
+            Navigator.of(context).pushReplacementNamed(FiltersScreen.routeName);
+          }),
         ],
       ),
     );
