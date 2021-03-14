9f01446f9d54dcf25e79ef3770859291e431bfe4
Daiki Iriguchi <iriguchi@filmingo.app>
2021-03-14 20:03:04 +0900

    add login function

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,12 +8,22 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> singup(String email, String password) async {
-    var url = Uri.parse(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDKHAMTjHgEq4asx9IwTMRb8SQ7oEmgdGU');
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url = Uri.parse(
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDKHAMTjHgEq4asx9IwTMRb8SQ7oEmgdGU');
     final response = await http.post(url,
         headers: {"Content-Type": "application/json"},
         body: json.encode(
             {'email': email, 'password': password, 'returnSecureToken': true}));
+    print(json.decode(response.body));
+  }
+
+  Future<void> singup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
